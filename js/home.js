252e0af407e7015df1648a5506ee95a012c1367e
8run0codev <br1_gio@hotmail.com>
2017-04-06 00:01:37 -0500

    Alpha 0.0.2
    Pagina del login y animaciones

--- js/home.js
+++ js/home.js
@@ -21,3 +21,90 @@ function navEnElTop() {
 		
 	});
 }
+
+//Login animacion
+
+function extenderRegistro() {
+	var registro = document.getElementById('registro'); 
+	var iniciar = document.getElementById('iniciar');
+
+	// eliminar clase  
+registro.classList.remove('m6');
+
+	// eliminar clase  
+iniciar.classList.remove('m6'); 	 	 
+
+	// agregar clase  
+registro.classList.add('m8');   
+ 
+ 	// agregar clase  
+iniciar.classList.add('m4');  
+
+
+
+}
+
+function regresarTamanoRegistro() {
+	var registro = document.getElementById('registro'); 
+	var iniciar = document.getElementById('iniciar'); 
+
+
+	// eliminar clase  
+registro.classList.remove('m8');
+
+	// eliminar clase  
+iniciar.classList.remove('m4'); 
+
+	// agregar clase  
+registro.classList.add('m6');   
+ 
+ 	// agregar clase  
+iniciar.classList.add('m6');  
+
+
+
+
+}
+
+function extenderIniciar() {
+	var registro = document.getElementById('registro'); 
+	var iniciar = document.getElementById('iniciar');
+
+
+	// eliminar clase  
+registro.classList.remove('m6');
+
+	// eliminar clase  
+iniciar.classList.remove('m6'); 	 	 
+
+	// agregar clase  
+registro.classList.add('m4');   
+ 
+ 	// agregar clase  
+iniciar.classList.add('m8'); 
+
+
+
+
+}
+
+function regresarTamanoIniciar() {
+	var registro = document.getElementById('registro'); 
+	var iniciar = document.getElementById('iniciar');
+
+
+	// eliminar clase  
+registro.classList.remove('m4');
+
+	// eliminar clase  
+iniciar.classList.remove('m8'); 
+
+	// agregar clase  
+registro.classList.add('m6');   
+ 
+ 	// agregar clase  
+iniciar.classList.add('m6');  
+
+
+
+}
